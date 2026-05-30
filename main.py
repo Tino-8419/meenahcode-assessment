cb3a07f23df1b63a0258aa3be54b27a3079fd272
Tino-8419 <146476120+Tino-8419@users.noreply.github.com>
2026-05-30 13:36:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 print("Name: Anero Anthony Chiagozie")
-print("School: University of Nigeria")
-print("Department: [Your Department]")
+print("School: University of Nigeria, Nsukka")
+print("Department: [Computer Science]")
 
 print("\nReason:")
 print("I want to become a Software Engineer and build solutions that solve real-world problems.")
