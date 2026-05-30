a0b0cc9d442eecac8c00c9bcbf7269d33d4051d5
Tino-8419 <146476120+Tino-8419@users.noreply.github.com>
2026-05-30 13:32:43 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Name: Anero Anthony Chiagozie")
+print("School: University of Nigeria")
+print("Department: [Your Department]")
+
+print("\nReason:")
+print("I want to become a Software Engineer and build solutions that solve real-world problems.")
